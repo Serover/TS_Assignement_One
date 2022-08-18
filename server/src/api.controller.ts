4bcda8e5beb59bc594ce88e7ffbf9c1241a54af9
Serover <35572082+Serover@users.noreply.github.com>
2022-08-18 14:36:08 +0200

    Moved from server to api.controller

---
+++ server/src/api.controller.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from "express";
+
+let myFakeServerDatabase: Meal[] = [
+  //TODO Make load from Json here insted?
+  { id: 0, name: "Kyckling gryta", protein: 40, calories: 1050 },
+  { id: 1, name: "Pew Pew", protein: 60, calories: 1200 },
+];
+
+export function updateMeal(req: Request<{ id: string }>, res: Response) {
+  const id: number = +req.params.id;
+  // TODO body validation?
+  const data = req.body;
+
+  //if ID exists
+  for (let i = 0; i < myFakeServerDatabase.length; i++) {
+    if (id === myFakeServerDatabase[i].id) {
+      //TODO async + save to local etc etc
+      myFakeServerDatabase[i] = data;
+
+      res.status(200).json(myFakeServerDatabase[i]);
+    } else {
+      // Some1 on the interet said 204 > 404
+      res.status(204);
+    }
+  }
+}
+
+export function deleteMeal(req: Request<{ id: string }>, res: Response) {
+  const id: number = +req.params.id;
+  // TODO body validation?
+
+  //if ID exists
+  for (let i = 0; i < myFakeServerDatabase.length; i++) {
+    if (id === myFakeServerDatabase[i].id) {
+      const deletedData = myFakeServerDatabase[i];
+      myFakeServerDatabase.splice(i, 1);
+
+      res.status(200).json(deletedData);
+    } else {
+      // Some1 on the interet said 204 > 404
+      res.status(204);
+    }
+  }
+}
+
+export function getAllMeals(req: Request, res: Response) {
+  res.status(200).json(myFakeServerDatabase);
+}
+
+export const addMeal = (req: Request, res: Response) => {
+  const data = req.body;
+  // TODO: save
+  res.status(201).json(data);
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,52 +1,16 @@
 const portNumb: number = 3000;
 const apiName: string = "/api/v1/";
 
-let myFakeServerDatabase: Meal[] = [
-  //TODO Make load from Json here insted?
-  { id: 0, name: "Kyckling gryta", protein: 40, calories: 1050 },
-  { id: 1, name: "Pew Pew", protein: 60, calories: 1200 },
-];
-
 import express, { Request } from "express";
+import { getAllMeals, updateMeal, deleteMeal, addMeal } from "./api.controller";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("Get Called");
-  res.status(200).json(myFakeServerDatabase);
-});
-
-app.post("/", (req, res) => {
-  console.log("Post Called");
-});
-
-app.put("/:id", (req: Request<{ id: string }>, res) => {
-  const id: number = +req.params.id;
-  // TODO body validation?
-  const data = req.body;
-
-  //if ID exists
-  for (let i = 0; i < myFakeServerDatabase.length; i++) {
-    if (id === myFakeServerDatabase[i].id) {
-      //TODO async + save to local etc etc
-      myFakeServerDatabase[i] = data;
-
-      res.status(200).json(myFakeServerDatabase[i]);
-    } else {
-      // Some1 on the interet said 204 > 404
-      res.status(204);
-    }
-  }
-});
-
-app.delete("/", (req: Request<{ id: string }>, res) => {
-  //if ID exists
-  //code 200, return object
-  // delete object
-  // else
-  // code 404
-});
+app.get("/", getAllMeals);
+app.post("/", addMeal);
+app.put("/:id", updateMeal);
+app.delete("/:id", deleteMeal);
 
 app.listen(portNumb, () =>
   console.log("Running on: http://localhost:" + portNumb)
